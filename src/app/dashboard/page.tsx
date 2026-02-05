1f61c4698b23e7dd0ca4343053e879f86310f52c
Alberto Játiva <134947748+aljanue@users.noreply.github.com>
2026-02-05 18:25:07 +0100

    stylized under construction dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,9 +2,11 @@ import UnderConstruction from "@/components/shared/under-construction";
 
 export default function HomePage() {
   return (
-    <UnderConstruction
-      title="Control Panel"
-      description="Your dashboard to manage subscriptions and expenses will be available soon."
-    />
+    <div className="flex items-center justify-center h-screen">
+      <UnderConstruction
+        title="Control Panel"
+        description="Your dashboard to manage subscriptions and expenses will be available soon."
+      />
+    </div>
   );
 }
